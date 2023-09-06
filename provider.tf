d5388302007516908fb204d1a9b40a4c781b6d3a
Ola Oruku <olaoruku007@gmail.com>
2023-09-06 01:19:38 -0400

    remove assume_role block

--- provider.tf
+++ provider.tf
@@ -1,9 +1,9 @@
 provider "aws" {
   region = "us-west-1"
-  assume_role {
-    role_arn     = "arn:aws:iam::812818276534:role/AWSAdmin"
-    session_name = "TerraformSession"
-  }
+  # assume_role {
+  #   role_arn     = "arn:aws:iam::812818276534:role/AWSAdmin"
+  #   session_name = "TerraformSession"
+  # }
 
 }
 
