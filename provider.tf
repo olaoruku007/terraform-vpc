a120fe4cf8083b225445b72628ff556cc81316bd
olaoruku007 <93499206+olaoruku007@users.noreply.github.com>
2024-02-24 02:19:56 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-2"
   # assume_role {
   #   role_arn     = "arn:aws:iam::812818276534:role/AWSAdmin"
   #   session_name = "TerraformSession"
